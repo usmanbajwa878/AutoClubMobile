5c26fb5ba74e5f9a09c24094a2bda51736596a8d
usmanbajwa <usmanbajwa878@gmail.com>
2022-08-12 02:38:29 +0500

    added husky

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+        "singleQuote": true,
+        "trailingComma": "all",
+        "printWidth": 120,
+        "tabWidth": 4,
+        "useTabs": true,
+        "semi": false
+    
+};
--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -12,9 +13,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
   },
 });
